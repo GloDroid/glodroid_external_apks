0757d7c69081a494abdcd84bba625943dbc96e69
Roman Stratiienko <r.stratiienko@gmail.com>
2022-11-16 16:45:55 +0200

    Add configuration files
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

---
+++ glodroid-apks.mk
@@ -0,0 +1,8 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/preinstall.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/preinstall.rc                       \
+    $(LOCAL_PATH)/preinstall.sh:$(TARGET_COPY_OUT_VENDOR)/etc/preinstall/preinstall.sh                 \
+    $(LOCAL_PATH)/fdroid.apk:$(TARGET_COPY_OUT_VENDOR)/etc/preinstall/fdroid.apk_all                   \
+    $(LOCAL_PATH)/shade-launcher3.apk:$(TARGET_COPY_OUT_VENDOR)/etc/preinstall/shade-launcher3.apk_all \
+    $(LOCAL_PATH)/skytube.apk:$(TARGET_COPY_OUT_VENDOR)/etc/preinstall/skytube.apk_all                 \
+    $(LOCAL_PATH)/fenix-arm.apk:$(TARGET_COPY_OUT_VENDOR)/etc/preinstall/fenix.apk_arm                 \
+    $(LOCAL_PATH)/fenix-arm64.apk:$(TARGET_COPY_OUT_VENDOR)/etc/preinstall/fenix.apk_arm64             \
